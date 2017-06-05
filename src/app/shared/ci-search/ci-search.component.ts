6da68db4d8e3e03f11df86613b89702b00520fab
crluo0929 <crluo0929@gmail.com>
2017-06-05 21:51:52 +0800

    add dynamic component with spec configuration

--- src/app/shared/ci-search/ci-search.component.ts
+++ src/app/shared/ci-search/ci-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CodeName } from "app/vo/CodeName";
 
 @Component({
   selector: 'ci-search',
@@ -7,20 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CiSearchComponent implements OnInit {
 
-  private name:string ;
-  private options=[{code:'1',name:'A'},{code:'2',name:'B'},{code:'3',name:'C'}]; //for test
+  private specs:{label:string,dynamicType:string,name:string,options?:CodeName[],required?:boolean,disabled?:boolean,p?:string}[] = [
+    {label:'輸入帳號',dynamicType:'InputText',name:'account',required:true, p:"user"},
+    {label:'輸入密碼',dynamicType:'InputPasswordText',name:'password',required:true,p:"lock"},
+    {label:'選擇角色',dynamicType:'SelectOneMenu',name:'role',options:[{code:'1',name:'IT政策管理者'},{code:'2',name:'財產管理者'}],required:true},
+    {label:'多選參數',dynamicType:'SelectManyMenu',name:'param',options:[{code:'1',name:'A'},{code:'2',name:'B'},{code:'3',name:'C'},{code:'4',name:'D'},{code:'5',name:'E'}]},
+  ]
+
   test(f){
     console.log(f.value);
   }
-  doLogin(f){
-    console.log(f.value);
-  }
-  testInput(e){
-    console.log(e);
-  }
-  changeSelectOne(e){
-    console.log(e);
-  }
+
 
   constructor() { }
 
--- src/app/shared/dynamic-html/dynamic-html.component.ts
+++ src/app/shared/dynamic-html/dynamic-html.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit, Input, Output, EventEmitter,forwardRef  } from '@ang
 import { CodeName } from "app/vo/CodeName";
 import { ControlValueAccessor,NG_VALUE_ACCESSOR } from "@angular/forms";
 
-
-//reference: https://segmentfault.com/a/1190000009070500
+/**
+ *  reference: http://blog.csdn.net/u010730126/article/details/70799099,
+               https://dotblogs.com.tw/kinanson/2017/04/15/211606
+ */
 
 const noop = () => { };
 
@@ -21,21 +23,32 @@ export const EXE_COUNTER_VALUE_ACCESSOR: any = {
 })
 export class DynamicHtmlComponent implements OnInit,ControlValueAccessor  {
 
+  //元件類型
   @Input() private dynamicType:string ;
+  //元件id
   @Input() private id?:string;
-  @Input() private name?:string;
+  //元件名稱(必填，須給ngModel綁定)
+  @Input() private name:string;
+  //元件必填屬性
   @Input() private required?:boolean=false;
+  @Input() private disabled: boolean = false;
+  @Input() private defaultValue ;
 
-  //輸入框
+  //輸入框值
   @Input() private _value?:string ='';
-
-  //下拉選單
+  //單選下拉值
   @Input() private _selected?:CodeName ;
+  //多選下拉值
   @Input() private _selects?:CodeName[]=[] ;
-  @Input() private options?:CodeName[]=[] ;
 
+
+  //下拉選單項目
+  @Input() private options?:CodeName[]=[] ;
+  //icon style
   @Input() private p?:string='';
+  //placeholder
   @Input() private placeholder?:string='';
+  //說明欄位
   @Input() private label?:string='' ;
 
   private icon_clazz:string ;
@@ -51,10 +64,14 @@ export class DynamicHtmlComponent implements OnInit,ControlValueAccessor  {
 
   private onTouchedCallback: () => void = noop;
   private onChangeCallback: (_: any) => void = noop;
-  private disabled: boolean = false;
+
 
   writeValue(obj: any): void {
-    this._value = obj ;
+    if(this.dynamicType==='InputText') this._value = obj ;
+    else if(this.dynamicType==='InputPasswordText') this._value = obj ;
+    else if(this.dynamicType==='SelectOneMenu') this._selected = obj ;
+    else if(this.dynamicType==='SelectManyMenu') this._selects = obj ;
+
   }
   registerOnChange(fn: any): void {
     this.onChangeCallback = fn ;
@@ -71,6 +88,7 @@ export class DynamicHtmlComponent implements OnInit,ControlValueAccessor  {
   }
 
   set value(v: any) {
+    console.log("set:"+v);
     if (v !== this._value) {
       this._value = v ;
       this.onChangeCallback(v);
@@ -82,27 +100,38 @@ export class DynamicHtmlComponent implements OnInit,ControlValueAccessor  {
   }
 
   set selected(v:any){
-    if (v !== this._selected) {
+    if (v != this._selected) {
       this._selected = v ;
       this.onChangeCallback(v);
     }
   }
 
+  get selects():any{
+    return this._selects ;
+  }
+
+  set selects(v:any){
+    if (v != this._selects) {
+      this._selects = v ;
+      this.onChangeCallback(v);
+    }
+  }
+
   //當元件裡面的元件有動作時，也要通知外部一起更新
-  input(event){
+  blur(event){
     this._value = event.target.value ;
+    console.log("change:"+this._value);
     this.onChangeCallback(event.target.value);
   }
 
   changeSelectOne(event){
-    console.log('改變了~~~'+JSON.stringify(this._selected));
-    console.log('改變了改變了~~~'+typeof event.target.value);
-    // this._selected = event.target.value ;
-    this.onChangeCallback(event.target) ;
+    //改變了，也要把值向外通知
+    this.onChangeCallback(this._selected) ;
+
   }
 
   changeSelectMany(event){
-    this.onChangeCallback(event.target) ;
+    this.onChangeCallback(this._selects) ;
   }
 
 
